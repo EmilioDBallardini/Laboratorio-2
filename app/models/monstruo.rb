2438d2bf1b3419d8f2a661208e097c817f1c7e64
EmilioDBallardini <qudit@DESKTOP-H96U6DS.localdomain>
2021-09-17 10:14:04 -0300

    Relaciones y destrucciones

--- app/models/monstruo.rb
+++ app/models/monstruo.rb
@@ -4,6 +4,10 @@ class Monstruo < ApplicationRecord
 	validates :nombre, presence: true, uniqueness: true
 
 	#Relaciones
-	has_many :victimas
+	has_many :victimas, dependent: :destroy
+
+
+	#Scopes
+	scope :eliminar, -> {  }
 
 end
--- app/models/victima.rb
+++ app/models/victima.rb
@@ -2,4 +2,8 @@ class Victima < ApplicationRecord
 
 	#Relaciones
 	belongs_to :monstruo
+
+	#Scopes
+	scope :mayor_25, -> { where("edad > 25") }
+
 end
